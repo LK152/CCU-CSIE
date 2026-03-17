59219c3e721b8959ae3d01ce504e56a8bcf15836
LK152 <wuluke0@gmail.com>
2026-03-17 20:37:23 +0800

    add

--- 115/0317/fib.c
+++ 115/0317/fib.c
@@ -1,11 +1,11 @@
 #include "stdio.h"
 
-long long table[91];
+long long table[90];
 
 void dp() {
+    table[0] = 1;
     table[1] = 1;
-    table[2] = 1;
-    for (int i = 3; i < 91; i++) {
+    for (int i = 2; i < 90; i++) {
         table[i] = table[i - 1] + table[i - 2];
     }
 }
@@ -16,10 +16,10 @@ char fib(int lv, long long pos) {
     if (lv == 2)
         return '1';
 
-    if (pos <= table[lv - 2]) {
+    if (pos <= table[lv - 3]) {
         return fib(lv - 2, pos);
     }
-    return fib(lv - 1, pos - table[lv - 2]);
+    return fib(lv - 1, pos - table[lv - 3]);
 }
 
 int main() {
